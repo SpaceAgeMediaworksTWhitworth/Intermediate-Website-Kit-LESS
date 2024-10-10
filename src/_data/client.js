7a9b1f76aeaea40c12e986f10e4f594ebdb62e8c
Space Age Mediaworks TW <tdwhitworth@spaceagemediaworks.com>
2024-10-10 16:32:02 -0500

    Initial Commit
    Kelly Kleen website using Intermediate Kit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Kelly Kleen Auto Detailing",
+    "email": "Kellykleenautodetailing@gmail.com",
+    "phoneForTel": "636-234-4211",
+    "phoneFormatted": "(636) 234-4211",
     "address": {
-        "lineOne": "First Address Line",
+        "lineOne": "49 Vemmer Drive",
         "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "city": "Union",
+        "state": "MO",
+        "zip": "63084",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "mapLink": "https://www.google.com/maps/place/Kelly+Kleen+Auto+Detailing/@38.4802307,-91.031462,10z/data=!3m1!4b1!4m6!3m5!1s0x87d945b92efec009:0x9036339058c3b4dd!8m2!3d38.480155!4d-90.7018235!16s%2Fg%2F11hn1wyttq?entry=ttu&g_ep=EgoyMDI0MTAwOC4wIKXMDSoASAFQAw%3D%3D"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/KellyKleenllc/",
+        "instagram": "https://www.instagram.com/kelly_kleen_detailing/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://kellykleenautodetailing.com"
 };
